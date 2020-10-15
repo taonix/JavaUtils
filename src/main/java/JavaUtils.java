3280cdc33bceba9d5f30ffc1c74dbfcf39adc303
Taonix <taonix314@gmail.com>
2020-10-15 13:24:48 +0200

    JSON update

--- src/main/java/JavaUtils.java
+++ src/main/java/JavaUtils.java
@@ -1,5 +1,10 @@
 import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class JavaUtils {
 
@@ -8,8 +13,29 @@ public class JavaUtils {
      * Helped by Automgen
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
     }
 
     /**
